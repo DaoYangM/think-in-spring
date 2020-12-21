76660dffc5d5e9f6de371c864443f129e0060169
deyangye <deyangye@gmail.com>
2020-12-21 23:48:05 +0800

    beanDefinition registry

--- dependency-lookup/src/main/java/top/daoyang/spring/dependency/lookup/ObjectProviderDemo.java
+++ dependency-lookup/src/main/java/top/daoyang/spring/dependency/lookup/ObjectProviderDemo.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.util.logging.Logger;
+
 /**
  * 通过{@link org.springframework.beans.factory.BeanFactory#getBeanProvider(Class)}
  * 返回{@link ObjectProvider} 类，可以间接、按需的获取Spring Bean
@@ -12,13 +14,13 @@ import org.springframework.context.annotation.Bean;
 public class ObjectProviderDemo {
 
     public static void main(String[] args) {
-
+        Logger logger = Logger.getLogger(ObjectProviderDemo.class.getName());
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ObjectProviderDemo.class);
 
         ObjectProvider<String> stringBeanProvider = applicationContext.getBeanProvider(String.class);
 
         for (String s : stringBeanProvider) {
-            System.out.println(s);
+            logger.info(s);
         }
 
     }
